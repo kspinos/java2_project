d8f73c2a4c53c22bb7e9a7d8f203843b6ba75bbe
kspinos <spinosdmst@gmail.com>
2019-11-22 04:08:41 +0200

    Add Login.java file

---
+++ Login.java
@@ -0,0 +1,78 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JFrame implements ActionListener {
+
+    JPanel panel;
+    JLabel user_label, password_label, message;
+    JTextField userName_text;
+    JPasswordField password_text;
+    JButton submit, cancel;
+
+
+    Login() {
+
+        // User Label
+        user_label = new JLabel();
+        user_label.setText("Username :");
+        userName_text = new JTextField();
+
+        // Password
+
+        password_label = new JLabel();
+        password_label.setText("Password :");
+        password_text = new JPasswordField();
+
+        // Submit
+
+        submit = new JButton("Submit");
+
+        panel = new JPanel(new GridLayout(3, 1));
+
+        panel.add(user_label);
+        panel.add(userName_text);
+        panel.add(password_label);
+        panel.add(password_text);
+
+        message = new JLabel();
+        panel.add(message);
+        panel.add(submit);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Adding the listeners to components..
+        submit.addActionListener(this);
+        add(panel, BorderLayout.CENTER);
+        setTitle("Please login!");
+        setSize(300, 100);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String userName = userName_text.getText();
+        String password = password_text.getText();
+        if (userName.trim().equals("admin") && password.trim().equals("admin")) {
+            System.exit(0);
+        } else {
+            message.setText("Invalid user");
+        }
+
+    }
+
+    public static void main(String[] args) {
+		new Login();
+	}
+}
